2e60c6088c71ffd71bc9b12967ac15771044752d
Theseu5 <chrisdoran84@live.com>
2016-05-12 13:49:11 +1000

    initial tests class for a380

---
+++ AircraftSimulator/src/asgn2Tests/A380Tests.java
@@ -0,0 +1,113 @@
+package asgn2Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by perseus on 12/05/16.
+ */
+public class A380Tests {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void cancelBooking() throws Exception {
+
+    }
+
+    @Test
+    public void confirmBooking() throws Exception {
+
+    }
+
+    @Test
+    public void finalState() throws Exception {
+
+    }
+
+    @Test
+    public void flightEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void flightFull() throws Exception {
+
+    }
+
+    @Test
+    public void flyPassengers() throws Exception {
+
+    }
+
+    @Test
+    public void getBookings() throws Exception {
+
+    }
+
+    @Test
+    public void getNumBusiness() throws Exception {
+
+    }
+
+    @Test
+    public void getNumEonomy() throws Exception {
+
+    }
+
+    @Test
+    public void getNumFirst() throws Exception {
+
+    }
+
+    @Test
+    public void getNumPassengers() throws Exception {
+
+    }
+
+    @Test
+    public void getNumPremium() throws Exception {
+
+    }
+
+    @Test
+    public void getPassengers() throws Exception {
+
+    }
+
+    @Test
+    public void getStatus() throws Exception {
+
+    }
+
+    @Test
+    public void hasPassenger() throws Exception {
+
+    }
+
+    @Test
+    public void initialState() throws Exception {
+
+    }
+
+    @Test
+    public void seatsAvailable() throws Exception {
+
+    }
+
+    @Test
+    public void upgradeBookings() throws Exception {
+
+    }
+
+}
