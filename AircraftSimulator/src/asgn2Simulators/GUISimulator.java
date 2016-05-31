d7698c3b00d4e64b862b519c4eab0dfb1b031d90
SaiWhiley <sai.whiley196@gmail.com>
2016-05-31 22:01:38 +1000

    -start gooey

--- AircraftSimulator/src/asgn2Simulators/GUISimulator.java
+++ AircraftSimulator/src/asgn2Simulators/GUISimulator.java
@@ -2,13 +2,21 @@
  * 
  * This file is part of the AircraftSimulator Project, written as 
  * part of the assessment for CAB302, semester 1, 2016. 
- * 
+ *
  */
 package asgn2Simulators;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
 
 /**
  * @author hogan
@@ -23,7 +31,17 @@ public class GUISimulator extends JFrame implements Runnable {
 	 */
 	public GUISimulator(String arg0) throws HeadlessException {
 		super(arg0);
-		// TODO Auto-generated constructor stub
+	}
+
+	private void createGUI(){
+		JButton button;
+
+		button = new JButton("He test me papa");
+
+
+		)
+
+
 	}
 
 	/* (non-Javadoc)
@@ -31,7 +49,7 @@ public class GUISimulator extends JFrame implements Runnable {
 	 */
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		createGUI();
 
 	}
 
@@ -39,7 +57,8 @@ public class GUISimulator extends JFrame implements Runnable {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		SwingUtilities.invokeLater(new GUISimulator("BorderLayout"));
 
 	}
 
